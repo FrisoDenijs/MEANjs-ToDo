189a6c7fd1cf906e8c579ccfc33d486b0a60265c
FrisoDenijs <friso1990@gmail.com>
2018-12-23 16:42:55 +0100

    changed server.js to conform to tutorial

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const todoRoute = require('./routes/todo.route'); // Imports routes for the products
+const todoRoute = require('./routes/todo.route'); // Imports routes for the todo
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://localhost:27017';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/todo', todoRoute);
 
 let port = 1234;
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-})
+    console.log('Server is up and running on port numner ' + port);
+});
